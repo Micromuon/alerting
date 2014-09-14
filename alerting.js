3228851caea719b2d028e7e607eecfaa8b17f14d
lottec <charlotte.cutts@bskyb.com>
2014-09-14 22:18:26 +0100

    initial commit

---
+++ alerting.js
@@ -0,0 +1,80 @@
+var databaseUrl = "alertingDB";
+var collections = ["settings"];
+var db = require("mongojs").connect(databaseUrl, collections);
+var redisPubSub = require("node-redis-pubsub-fork"),
+    pubsubChannel = new redisPubSub({ scope: "messages" });
+var moment = require('moment');
+moment().format();
+
+var nodemailer = require('nodemailer');
+// from http://www.nodemailer.com/
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'sea.microservices@gmail.com',
+            pass: 'testalerting'
+        }
+    });
+
+var emailTracker = {};
+
+pubsubChannel.on("healthcheck:failed", function(data) {
+
+    db.settings.find({"name": data.name}).toArray(function(err, result) {
+        if(!err && result.length > 0 && result[0].emails.length > 0) {
+            if((emailTracker[data.name] == null) || (moment(data.time).diff(emailTracker[data.name], 'seconds') >= result[0].frequency)) {
+                // setup e-mail data with unicode symbols
+                var mailOptions = {
+                    from: 'SEA Microservices <sea.microservices@gmail.com>', // sender address
+                    to: result[0].emails.toString(), // list of receivers
+                    subject: "Microservice " + data.name + " failure!", // Subject line
+                    text: 'The microservice '+ data.name + ' is failing its healthchecks!', // plaintext body
+                };
+                // send mail with defined transport object
+                transporter.sendMail(mailOptions, function(error, info){
+                    if(error) {
+                        pubsubChannel.emit("alerting:failed", {name: data.name, message: "Warning email for " + data.name + " failed to send."});
+                        console.log(error);
+                    } else{
+                        pubsubChannel.emit("alerting:sent", {name: data.name, message: "email sent"});
+                        console.log('Message sent: ' + info.response);
+                        emailTracker[data.name] = moment();
+                    }
+                }); 
+            }
+        } else {
+            console.log("No alerts data found");
+        }       
+    });
+
+});
+
+pubsubChannel.on("alerting:saveInfo", function(data) {
+    db.collection("settings").update(
+        { name: data.name },
+        { name: data.name, emails: data.emails, frequency: data.frequency },
+        { upsert: true },
+        function(err, object) {
+            if (err) {
+                console.warn(err.message);
+                data.failed = "true";
+                pubsubChannel.emit("alerting:saveResult", data);
+            } else {
+                console.log("Alerts entry saved successfully");
+                pubsubChannel.emit("alerting:saveResult", data);
+            }
+        }
+    );
+});
+
+pubsubChannel.on("alerting:queryAlerting", function(data) {
+    db.settings.find({"name": data.name}).toArray(function(err, queryAlertRes) {
+        if (!err) {
+            pubsubChannel.emit("alerting:queryAlertingResult", {"name": data.name, "alertSettings": queryAlertRes});
+        }
+        else {
+            pubsubChannel.emit("alerting:queryAlertingResult", {"name": data.name, "alertSettings": []});
+        }
+    });
+});
---
+++ test/testAlerting.js
@@ -0,0 +1,171 @@
+var databaseUrl = "alertingDB"; // "username:password@example.com/mydb"
+var collections = ["settings"];
+var db = require("mongojs").connect(databaseUrl, collections);
+var assert = require("assert"),
+    redisPubSub = require("node-redis-pubsub-fork"),
+    pubsubChannel = new redisPubSub({ scope: "messages" }),
+    timers = require("timers");
+require("../alerting.js");
+var CronJob = require('cron').CronJob;
+var moment = require('moment');
+moment().format();
+
+after(function(){
+    db.settings.drop();
+});
+
+var newJob;
+
+after(function() {
+    newJob.stop();
+});
+      
+describe("test alerting", function() {  
+
+    it('doesn\'t try and send an email if no email address is configured', function(done) {
+        this.timeout(7000);
+        var success = true;
+        pubsubChannel.once("alerting:*", function(data){
+            success = false;
+        });
+        setTimeout(function() {
+            if (success) done();
+        }, 4000);
+        
+        pubsubChannel.emit("healthcheck:failed", {name: "testService", url: "testUrl", expectedResBody: "testExpectedResBody", actualResBody: "testActualResBody", expectedResStatus: "testExpectedResStatus", actualResStatus: "actualResStatus", result: "failing", time: moment() });
+    });
+    
+    it('allows services to have individual profiles', function(done){
+      this.timeout(7000);
+      pubsubChannel.onceIf("alerting:saveResult", function(data) {
+        db.settings.find({"name":data.name}).toArray(function(err, result) {
+            if (data.name == result[0].name) { done(); }
+        });
+      }, "failed", null);     
+        
+      pubsubChannel.emit("alerting:saveInfo", {name: "testService", emails: ["sea.microservices2@gmail.com", "sea.microservices@gmail.com"], frequency: "10"});
+    });
+    
+    it('allows querying of alert settings by service name', function(done) {
+        this.timeout(7000);
+        pubsubChannel.once("alerting:queryAlertingResult", function(data) {
+            db.settings.find({"name": "testService"}).toArray(function(err, result) {
+                console.log(result);
+                    if (err || !result) {
+                        console.log("No alerting job found.");
+                    } else {
+                        assert.equal(result[0].name, data.name);
+                        assert.equal(result[0].emails[0], data.alertSettings[0].emails[0]);
+                        assert.equal(result[0].emails[1], data.alertSettings[0].emails[1]);
+                        assert.equal(result[0].frequency, data.alertSettings[0].frequency);
+                        done();
+                    }
+                });
+        });
+        pubsubChannel.emit("alerting:queryAlerting", {name: "testService"});
+    });
+    
+    it('sends email on receipt of healthcheck:failed message', function(done){
+        this.timeout(7000);
+        pubsubChannel.once("alerting:sent", function(data){
+            done();
+        });
+        
+        pubsubChannel.emit("healthcheck:failed", {name: "testService", url: "testUrl", expectedResBody: "testExpectedResBody", actualResBody: "testActualResBody", expectedResStatus: "testExpectedResStatus", actualResStatus: "actualResStatus", result: "failing", time: moment()});  
+    });
+    
+    it('sends multiple emails according to frequency set, not healthcheck frequency', function(done) {
+        this.timeout(32000);
+        
+        var sentEmails = 0;
+        
+        pubsubChannel.on("alerting:sent", function(data) {
+            sentEmails ++;
+            if(sentEmails >= 2) {
+                done();
+            }
+        });
+        newJob = new CronJob("*/5 * * * * *", function(){
+            pubsubChannel.emit("healthcheck:failed", {name: "testService", url: "testUrl", expectedResBody: "testExpectedResBody", actualResBody: "testActualResBody", expectedResStatus: "testExpectedResStatus", actualResStatus: "actualResStatus", result: "failing", time: moment()}); 
+        }, null, true);
+    });
+                                                                             
+    it('allows updating of entries in the alerts database', function(done) {
+        this.timeout(7000);        
+        pubsubChannel.onceIf("alerting:saveResult", function(data) {
+            db.settings.find({"name":data.name}).toArray(function(err,result) {
+                console.log(data);
+                console.log(result);
+                if (data.name == result[0].name && equals(data.emails, result[0].emails)) { done(); }
+            });
+        }, "failed", null);
+        pubsubChannel.emit("alerting:saveInfo", {name: "testService", emails: [], frequency: "1"});
+    });
+    
+    it('sends failed message on receipt of healthcheck:failed message with invalid saved email', function(done) {
+        this.timeout(7000);
+        pubsubChannel.once("alerting:failed", function(data){
+            done();
+        });
+        pubsubChannel.emit("alerting:saveInfo", {name: "testService", emails: ["foo"], frequency: "1"});
+        pubsubChannel.onceIf("alerting:saveResult", function(data) {
+            pubsubChannel.emit("healthcheck:failed", {name: "testService", url: "testUrl", expectedResBody: "testExpectedResBody", actualResBody: "testActualResBody", expectedResStatus: "testExpectedResStatus", actualResStatus: "actualResStatus", result: "failing", time: moment()});
+        }, "failed", null);
+    });
+    
+});
+
+
+
+
+
+function equals ( x, y ) {
+    // If both x and y are null or undefined and exactly the same
+    if ( x === y ) {
+        return true;
+    }
+
+    // If they are not strictly equal, they both need to be Objects
+    if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) {
+        return false;
+    }
+
+    // They must have the exact same prototype chain, the closest we can do is
+    // test the constructor.
+    if ( x.constructor !== y.constructor ) {
+        return false;
+    }
+
+    for ( var p in x ) {
+        // Inherited properties were tested using x.constructor === y.constructor
+        if ( x.hasOwnProperty( p ) ) {
+            // Allows comparing x[ p ] and y[ p ] when set to undefined
+            if ( ! y.hasOwnProperty( p ) ) {
+                return false;
+            }
+
+            // If they have the same strict value or identity then they are equal
+            if ( x[ p ] === y[ p ] ) {
+                continue;
+            }
+
+            // Numbers, Strings, Functions, Booleans must be strictly equal
+            if ( typeof( x[ p ] ) !== "object" ) {
+                return false;
+            }
+
+            // Objects and Arrays must be tested recursively
+            if ( !equals( x[ p ],  y[ p ] ) ) {
+                return false;
+            }
+        }
+    }
+
+    for ( p in y ) {
+        // allows x[ p ] to be set to undefined
+        if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) {
+            return false;
+        }
+    }
+    return true;
+}
